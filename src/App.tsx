3691041fac417fca157d4494dc907dfc83322cec
Lucas Alves <lucasdolohov@gmail.com>
2024-12-30 22:46:54 -0300

    contador

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Lucas Doloh0v</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          contador {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
